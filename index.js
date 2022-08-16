641ea63ec28936720b0f62f880c8d20c177bcee4
ismailchakrane <ism.chakrane@gmail.com>
2022-08-16 16:24:49 +0100

    just commit

--- index.js
+++ index.js
@@ -6,7 +6,9 @@
     })
   })()
 
-show = (object) => document.getElementById(`button${object.id}`).click();
+const show = (object) => document.getElementById(`button${object.id}`).click();
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+const touch = (object) => obj.addEventListener("touchstart", () => console.log(object.id), false);
